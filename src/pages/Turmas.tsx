d85c9c35c2beaad1cee6d52caf440a74e183b05c
Juliana Moura <juliana_moura@id.uff.br>
2025-10-08 20:08:59 -0300

    feat: upgrade turmas page

--- src/pages/Turmas.tsx
+++ src/pages/Turmas.tsx
@@ -1,5 +1,25 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import useListarTurmas from "../hooks/listarTurmas";
+
+type Professor = {
+  id: string | number;
+  nome: string;
+  email: string;
+};
+
+type Disciplina = {
+  id: string | number;
+  nome: string;
+  cargaHoraria: number;
+};
+
+type Turma = {
+  id: string | number;
+  ano: number;
+  periodo: string;
+  professor: Professor;
+  disciplina: Disciplina;
+};
 
 export default function TurmasPage() {
   const { data, isLoading, error } = useListarTurmas();
@@ -8,12 +28,34 @@ export default function TurmasPage() {
   if (error) return <p>Erro: {(error as Error).message}</p>;
 
   return (
-    <ul>
-      {data?.map((turma: any) => (
-        <li key={turma.id}>
-          {turma.id} — <Link to={`/turmas/${turma.id}`}>ver detalhes</Link>
-        </li>
-      ))}
-    </ul>
+    <div style={{ padding: 20 }}>
+      <h3 style={{ marginBottom: 15 }}>Turmas</h3>
+      <table className="default-table">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Ano</th>
+            <th>Período</th>
+            <th>Disciplina</th>
+            <th>Professor</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((turma: Turma) => (
+            <tr key={turma.id}>
+              <td>{turma.id}</td>
+              <td>{turma.ano}</td>
+              <td>{turma.periodo}</td>
+              <td>{turma.disciplina?.nome}</td>
+              <td>{turma.professor?.nome}</td>
+              <td>
+                <NavLink to={`/turmas/${turma.id}`}>Ver detalhes</NavLink>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
