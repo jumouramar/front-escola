211e5f5fcfb83cb34967b55312b5f31a8ec09384
Juliana Moura <juliana_moura@id.uff.br>
2025-10-08 19:58:58 -0300

    feat: upgrade alunos page

--- src/pages/Alunos.tsx
+++ src/pages/Alunos.tsx
@@ -1,5 +1,11 @@
 import useListarAlunos from "../hooks/listarAlunos";
 
+type Aluno = {
+  id: string;
+  nome: string;
+  email: string;
+};
+
 export default function AlunosPage() {
   const { data, isLoading, error } = useListarAlunos();
 
@@ -7,10 +13,26 @@ export default function AlunosPage() {
   if (error) return <p>Erro: {(error as Error).message}</p>;
 
   return (
-    <ul>
-      {data?.map((aluno: any) => (
-        <li key={aluno.id}>{aluno.nome}</li>
-      ))}
-    </ul>
+    <div style={{ padding: 20 }}>
+      <h3 style={{ marginBottom: 15 }}>Alunos</h3>
+      <table className="default-table">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Nome</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((aluno: Aluno) => (
+            <tr key={aluno.id}>
+              <td>{aluno.id}</td>
+              <td>{aluno.nome}</td>
+              <td>{aluno.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
