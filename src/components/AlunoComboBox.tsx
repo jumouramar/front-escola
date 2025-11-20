aae4ff9b30badc31227e3eb70004ab53c6a011d0
Juliana Moura <juliana_moura@id.uff.br>
2025-11-20 19:37:51 -0300

    feat: add aluno combo box

---
+++ src/components/AlunoComboBox.tsx
@@ -0,0 +1,36 @@
+import { useInscricaoStore } from "../store/EnrollmentStore";
+import { useAlunosNaoInscritos } from "../hooks/useAlunosNaoInscritos";
+
+export function AlunoComboBox() {
+  const { turmaIdSelecionada, alunoIdSelecionado, setAlunoIdSelecionado } =
+    useInscricaoStore();
+
+  const {
+    data: alunos,
+    isLoading,
+    error,
+  } = useAlunosNaoInscritos(turmaIdSelecionada);
+
+  if (!turmaIdSelecionada) {
+    return <div>Selecione uma turma primeiro.</div>;
+  }
+
+  if (isLoading) return <div>Carregando alunos...</div>;
+  if (error) return <div>Erro ao carregar alunos.</div>;
+
+  return (
+    <select
+      value={alunoIdSelecionado ?? ""}
+      onChange={(e) =>
+        setAlunoIdSelecionado(e.target.value ? Number(e.target.value) : null)
+      }
+    >
+      <option value="">Selecione um aluno</option>
+      {alunos?.map((a) => (
+        <option key={a.id} value={a.id}>
+          {a.nome}
+        </option>
+      ))}
+    </select>
+  );
+}
