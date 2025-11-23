c638ad3ab7530f4a37ccb60b716f83a87eb07707
Juliana Moura <juliana_moura@id.uff.br>
2025-11-23 18:04:27 -0300

    feat: reorganize navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,9 @@
 import { NavLink } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-const NavBar = () => {
+export default function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg">
+    <nav className="navbar navbar-expand-lg custom-bg">
       <div className="container">
         <NavLink className="navbar-brand" to="/">
           Escola
@@ -19,42 +19,69 @@ const NavBar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/alunos">
-                <i className="bi bi-person"></i> Listar Alunos
-              </NavLink>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <i className="bi bi-person"></i> Alunos
+              </a>
+              <ul className="dropdown-menu dropdown-menu-dark">
+                <li>
+                  <NavLink className="dropdown-item" to="/alunos">
+                    <i className="bi bi-card-list"></i> Listar
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-item" to="/student_registration">
+                    <i className="bi bi-person-plus"></i> Cadastrar
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-item" to="/student_enrollment">
+                    <i className="bi bi-clipboard"></i> Inscrever
+                  </NavLink>
+                </li>
+              </ul>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/student_registration">
-                <i className="bi bi-person-plus"></i> Cadastrar Alunos
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/student_enrollment">
-                <i className="bi bi-clipboard"></i> Inscrição de Aluno
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/turmas">
-                <i className="bi bi-card-list me-1"></i> Listar Turmas
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/search">
-                <i className="bi bi-search"></i> Buscar Turmas
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/manage_class">
-                <i className="bi bi-clipboard-data"></i> Gerenciar Turmas
-              </NavLink>
+
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <i className="bi bi-card-list"></i> Turmas
+              </a>
+              <ul className="dropdown-menu dropdown-menu-dark">
+                <li>
+                  <NavLink className="dropdown-item" to="/turmas">
+                    <i className="bi bi-card-list"></i> Listar
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-item" to="/search">
+                    <i className="bi bi-search"></i> Buscar
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-item" to="/manage_class">
+                    <i className="bi bi-clipboard-data"></i> Gerenciar
+                  </NavLink>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
       </div>
     </nav>
   );
-};
-export default NavBar;
+}
